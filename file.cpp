52b3506ab92402f3ed175113c6385cf51c247c75
abrar-nazib <abrarnazib@gmail.com>
2023-10-18 16:43:25 +0600

    Create template file

---
+++ file.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//-------- typedefs -------
+typedef unsigned int ui;
+typedef long long ll;
+typedef unsigned long long llu;
+//------- /typedefs--------
+
+//-------- functions -------
+template <typename T>
+bool is_even(T num)
+{
+  return !(num & 1);
+}
+//------- /functions--------
+
+void solve()
+{
+  // Solution
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  long long T;
+  cin >> T;
+  while (T--)
+  {
+    solve();
+  }
+
+  return 0;
+}
